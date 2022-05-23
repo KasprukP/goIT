4cc3dd5749741d5e836715637eb3f34e9de2cf11
YUKON Pavel K <itflvby1991>
2022-05-23 12:58:30 +0300

    #2 Hello World st 73

--- src/java_lesson.java
+++ src/java_lesson.java
@@ -1,5 +1,16 @@
 public class java_lesson {
     public static void main(String[] args) {
         System.out.println("Hello ");
+
+        int vocationDays = 12;
+        System.out.println(vocationDays);
+        int x =12;
+        int a = 4;
+
+        double y = Math.pow (x,a);
+        System.out.println(y);
+
+        double z = Math.sqrt(4);
+        System.out.println("Квадрат "+ z );
     }
 }
