d8f971a0e9d118aa76f96cd8f4e11ea2d0b69f9f
kaspruk_p <kaspruk.p@gmail.com>
2022-06-20 22:09:59 +0300

    #4 Lesson11(Homework done)

--- src/udemyLessons/HomeworkLesson11/Car.java
+++ src/udemyLessons/HomeworkLesson11/Car.java
@@ -3,18 +3,35 @@ package udemyLessons.HomeworkLesson11;
 public class Car {
     String color;
     String engine;
-    int doors;
+    int doorsCount;
 
 
-    Car(String color,String engine,int doors){
-        this.color=color;
-        this.engine=engine;
-        this.doors=doors;
+   public Car(String color, String engine, int doorsCount) {
+        this.color = color;
+        this.engine = engine;
+        this.doorsCount = doorsCount;
 
     }
 
+    static class CarTest {
+        void changeDoors(Car c, int doorsCount) {
+            c.doorsCount = doorsCount;
+        }
+
+        void changeColor(Car c1, Car c2) {
+            String color = c1.color;
+            c1.color = c2.color;
+            c2.color = color;
+        }
+    }
+
     public static void main(String[] args) {
-Car c1 = new Car("krasnuj","v12",4);
-        System.out.println(c1.color);
+        CarTest ct = new CarTest();
+        Car car1 = new Car("krasnuj", "v12", 4);
+        Car car2 = new Car("Blue", "W6", 2);
+        ct.changeDoors(car1,3);
+        ct.changeColor(car1,car2);
+        System.out.println("information "+car1.color+ " Engine "+car1.engine+"Doors "+car1.doorsCount);
+
     }
 }
