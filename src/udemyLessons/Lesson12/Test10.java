ab59e33b0bff31339c279b1cfb0c472a7bfbfc2c
kaspruk_p <kaspruk.p@gmail.com>
2022-06-21 19:27:06 +0300

    #4 Lesson11(Homework done)

---
+++ src/udemyLessons/Lesson12/Test10.java
@@ -0,0 +1,10 @@
+package udemyLessons.Lesson12;
+
+public class Test10 {
+    public static void main(String[] args) {
+        int a=40;
+        if (a<30)
+            System.out.println("a menshe 20");
+        
+    }
+}
