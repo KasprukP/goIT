875db5e45cdc7bc29fd308412796619830f7e984
YUKON Pavel K <itflvby1991>
2022-07-07 16:48:46 +0300

    Lesson 14 (done)

---
+++ src/udemyLessons/HomeworkLesson17/Test1.java
@@ -0,0 +1,18 @@
+package udemyLessons.HomeworkLesson17;
+
+public class Test1 {
+    public static boolean ravenstvo(StringBuilder s1, StringBuilder s2) {
+
+        return (s1.toString().equals(s2.toString()));
+    }
+
+
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("privet");
+        StringBuilder sb2 = new StringBuilder("privet");
+        System.out.println(ravenstvo(sb1, sb2));
+
+    }
+}
+
+
