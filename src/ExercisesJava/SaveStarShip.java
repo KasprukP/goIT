8b766849b8752048a8cdb92483c4246740a19427
YUKON Pavel K <itflvby1991>
2022-09-02 17:47:20 +0300

    Lesson  (SaveStarShip)

--- src/ExercisesJava/SaveStarShip.java
+++ src/ExercisesJava/SaveStarShip.java
@@ -1,4 +1,21 @@
 package ExercisesJava;
 
 public class SaveStarShip {
-}
+    public int calculateDistance(int distance) {
+        if (distance > 0) {
+            return distance;
+        } else {
+            return -distance;
+        }
+    }
+
+
+            //Test output
+            public static void main(String[] args) {
+                SaveStarShip ship = new SaveStarShip();
+
+                //Should be 10
+                System.out.println(ship.calculateDistance(-10));
+            }
+        }
+
