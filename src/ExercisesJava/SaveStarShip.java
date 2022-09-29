2b82d4c4e6b8c632d35eca4fb9d7214a1932bafb
YUKON Pavel K <itflvby1991>
2022-09-29 17:22:08 +0300

    Lesson  (SaveStarShip)

--- src/ExercisesJava/SaveStarShip.java
+++ src/ExercisesJava/SaveStarShip.java
@@ -1,6 +1,8 @@
 package ExercisesJava;
 
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class SaveStarShip {
     public int calculateDistance(int distance) {
@@ -45,24 +47,80 @@ public class SaveStarShip {
         } else {
 
         }
-        return count*50;
+        return count * 50;
     }
 
+    public int roundSpeed(int speed) {
 
+        return speed - speed % 10;
+    }
+
+    public int calculateNeededFuel(int distance) {
+        if (distance < 20) {
+            return 1000;
+        } else if (distance > 20) {
+
+        }
+        return (distance - 20) * 5 + 1000;
+    }
+
+    public void calculateMaxPower() {
+        Scanner in = new Scanner(System.in);
+        int power1 = in.nextInt();
+        int power2 = in.nextInt();
+        int power3 = in.nextInt();
+        int superpower = IntStream.of(power1, power2, power3).reduce(Math::max).getAsInt();
+        float k1 = 0.7f;
+        float k2 = 1.2f;
+        float k3 = 2.1f;
+        if (superpower < 10) {
+            System.out.println(superpower * k1);
+        } else if (10 <= superpower && superpower < 100) {
+            System.out.println(superpower * k2);
+        } else if (superpower > 100) {
+            System.out.println(superpower * k3);
+            in.close();
+        }
+    }
+
+    public String getMyPrizes(int ticket) {
+        if (ticket % 10 == 0) {
+            return "crystall";
+        } else if (ticket % 10 == 7) {
+            return "chip";
+        } else if (ticket > 200) {
+            return "coin";
+        }
+return " ";
+    }
+
+}
+
+    }
 //Test output
-public static void main(String[] args) {
-    SaveStarShip ship = new SaveStarShip();
+public static void main(String[]args){
+        SaveStarShip ship=new SaveStarShip();
 
-    //Should be 10
-    System.out.println(ship.calculateDistance(-10));
+        //Should be 10
+        System.out.println(ship.calculateDistance(-10));
 
-    //Should be [Fobius, Demius]
-    System.out.println(Arrays.toString(ship.getPlanets("DangerBanger")));
+        //Should be [Fobius, Demius]
+        System.out.println(Arrays.toString(ship.getPlanets("DangerBanger")));
 
-    //Should be Earth
-    System.out.println(ship.choosePlanet(10));
+        //Should be Earth
+        System.out.println(ship.choosePlanet(10));
 
-    //Should be 700
-    System.out.println(ship.calculateFuelPrice("STAR100", 10));
-}
-}
+        //Should be 700
+        System.out.println(ship.calculateFuelPrice("STAR100",10));
+
+        //Should be 60
+        System.out.println(ship.roundSpeed(55));
+
+        //Should be 1005
+        System.out.println(ship.calculateNeededFuel(1001));
+
+        //Test stdin data - 1 3 5.
+        //Console ouput should be 3.5
+        ship.calculateMaxPower();
+        }
+        }
