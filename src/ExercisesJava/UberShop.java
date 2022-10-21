257a000bd7f8f47c3b160aa7c75e16a6842ca989
YUKON Pavel K <itflvby1991>
2022-10-21 17:38:07 +0300

    UberShop (for)

--- src/ExercisesJava/UberShop.java
+++ src/ExercisesJava/UberShop.java
@@ -3,20 +3,31 @@ package ExercisesJava;
 import java.util.Arrays;
 
 public class UberShop {
-    public void printPrices(float[] prices) {
+    public void multiplyPrices(float[] prices) {
 
-        for (int i = 0; i < prices.length; i++){
-            System.out.println(prices[i] + " jup.");
+        for (int i = 0; i <prices.length; i++ ) {
+            if (i < 1000) {
+                prices[i] = prices[i]*2;
+
+
+            }
+            else if (i>=100){
+                prices[i] =  (prices[i]*1.5f);
+            }
+
         }
     }
+
+
 
     //Test output
     public static void main(String[] args) {
         UberShop shop = new UberShop();
 
-        //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
-        float[] prices = new float[] {100f, 23.5f, 400f};
-        shop.printPrices(prices);
+        //Should be [200, 2250]
+        float[] prices = new float[]{100f, 1500f};
+        shop.multiplyPrices(prices);
+        System.out.println(Arrays.toString(prices));
     }
 }
 
